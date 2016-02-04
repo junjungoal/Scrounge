472c21172fa881d75051a57242d4e8aa2ad98748
山田　潤 <junyamada107@gmail.com>
2016-02-04 23:49:07 +0900

    init commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.5'
+gem 'mysql2', '>= 0.3.13', '< 0.5'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'haml-rails'
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'byebug'
+  gem 'bullet'
+  gem "rspec-rails", "~> 3.1.0"
+  gem "factory_girl_rails", "~> 4.4.1"
+  gem "faker"
+end
+
+group :development do
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+end
---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Scrounge
+  class Application < Rails::Application
+    config.generators do |g|
+      g.template_engine      false
+      g.test_framework       :rspec, view_specs: false, helper_specs: false, fixture: true
+      g.helper               false
+      g.stylesheets          false
+      g.javascripts          false
+    end
+
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
